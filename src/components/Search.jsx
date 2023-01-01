8c4ed1a36d10b764fb6bae45786950a58193900e
Alexandar Valov <alexvulov123@gmail.com>
2023-01-01 21:24:46 +0200

    Implemented a way to search a user.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setErr(true);
+      console.log(err.message);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
   return (
     <div className="border-b-2 border-[#f2f2f2]">
       <div className="flex justify-center items-center w-full border-b-2 border-[#f2f2f2]">
         <div className="p-2 py-4 font-semibold text-lg w-[100%]">
           Search Friends
         </div>
-        <form className="relative p-2 focus-within:w-full">
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className="relative p-2 focus-within:w-full"
+        >
           <input
             type="text"
             className="relative bg-[#fff] text-[#161616] w-8 h-8 rounded-full border border-black focus:w-full focus:border-black outline-none cursor-pointer focus:cursor-text pl-8 py-4"
             placeholder="Search a user"
+            onKeyDown={handleKey}
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
           />
           <AiOutlineSearch
             size={20}
@@ -20,17 +53,19 @@ const Search = () => {
           />
         </form>
       </div>
-      <div className="p-2 flex items-center gap-2 cursor-pointer text-[#161616] hover:bg-[#f2f2f2]">
-        <img
-          src="https://images.pexels.com/photos/14769547/pexels-photo-14769547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""
-          className="w-[50px] h-[50px] rounded-full object-cover"
-        />
-        <div>
-          <span className="font-semibold">Jane</span>
-          <p className="text-sm opacity-80">Fck you</p>
+
+      {user ? (
+        <div className="p-2 flex items-center gap-2 cursor-pointer text-[#161616] hover:bg-[#f2f2f2]">
+          <img
+            src={user.photoURL}
+            alt=""
+            className="w-[50px] h-[50px] rounded-full object-cover"
+          />
+          <div>
+            <span className="font-semibold">{user.displayName}</span>
+          </div>
         </div>
-      </div>
+      ) : null}
     </div>
   );
 };
