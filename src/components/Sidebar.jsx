07b3ef2ddde8910ab0da985afd795b587c14c3d4
Alexandar Valov <alexvulov123@gmail.com>
2023-01-28 22:15:27 +0200

    Deleted the unneccesary code that was left. There are just some final touches to be made and the webside will be ready for production.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,14 +5,6 @@ import Chats from "./Chats";
 
 const Sidebar = ({ user }) => {
   return (
-    // <div className="md:w-[40%] w-full bg-[#fff] md:h-full h-[50%] xlg:rounded-2xl">
-    //   <Navbar />
-    //   <Search />
-    //   <div className="overflow-y-scroll overflow-x-hidden chats md:chats2">
-    //     <Chats />
-    //   </div>
-    // </div>
-
     <div
       className={`${
         user ? "sidebarWithUser w-full" : "sidebarWithoutUser w-full"
