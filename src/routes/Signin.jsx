362daee469fd62887625ef09006e70f4bc90ef4f
Alexandar Valov <alexvulov123@gmail.com>
2023-01-30 21:51:03 +0200

    Changed the UI of the Sign In and Sign Up pages.

--- src/routes/Signin.jsx
+++ src/routes/Signin.jsx
@@ -21,40 +21,44 @@ const Login = () => {
   };
 
   return (
-    <div className="bg-[#f2f2f2] h-[100vh] flex items-center">
-      <div className="mx-auto px-12 py-12 bg-[#fff] rounded-2xl">
+    <div className="bg-[#202020] h-[100vh] flex items-center">
+      <div className="mx-auto px-12 py-12 bg-[#161616] rounded-2xl">
         <div className="flex flex-col gap-4">
-          <span className="text-2xl font-bold text-[#161616] mx-auto w-full text-center">
+          <span className="text-2xl font-bold text-[#fff] mx-auto w-full text-center">
             Coffee Talks
           </span>
-          <span className="text-lg font-bold text-[#161616] mx-auto">
-            Sign In
-          </span>
+          <span className="text-lg font-bold text-[#fff] mx-auto">Sign In</span>
         </div>
         <form onSubmit={handleSubmit}>
           <div className="my-4">
-            <label className="text-[#161616]">Email</label>
+            <label for="email" className="text-[#fff]">
+              Email
+            </label>
             <div className="my-2 w-full relative rounded-2xl shadow-xl">
               <input
-                className="w-full p-2 border rounded-2xl bg-[#fff] text-[#161616]"
+                id="email"
+                className="w-full p-2 border rounded-2xl bg-[#161616] text-[#fff] outline-none"
                 type="email"
               />
             </div>
           </div>
           <div className="my-4">
-            <label className="text-[#161616]">Password</label>
+            <label for="password" className="text-[#fff]">
+              Password
+            </label>
             <div className="my-2 w-full relative rounded-2xl shadow-xl">
               <input
-                className="w-full p-2 border rounded-2xl bg-[#fff] text-[#161616]"
+                id="password"
+                className="w-full p-2 border rounded-2xl bg-[#161616] text-[#fff] outline-none"
                 type="password"
               />
             </div>
           </div>
-          <button className="w-full my-2 p-3 bg-[#161616] text-[#fff] hover:opacity-50 rounded-2xl shadow-xl font-bold">
+          <button className="w-full my-2 p-3 bg-[#161616] text-[#fff] hover:opacity-50 rounded-2xl shadow-xl font-bold border border-[#161616]">
             Sign In
           </button>
         </form>
-        <p className="mt-4 text-center text-[#161616]">
+        <p className="mt-4 text-center text-[#fff]">
           Don't have an account?<span> </span>
           <Link className="hover:opacity-50 font-bold" to="/signup">
             Sign Up
